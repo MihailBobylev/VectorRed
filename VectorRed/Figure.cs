3a2f1e96fb9d1059890882889161bcc7f399cec3
sdremchenko <sdremchenko@mmtr.ru>
2020-11-08 15:50:25 +0300

    Созданы фигуры

---
+++ VectorRed/Figure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorRed
+{
+	abstract class Figure
+	{
+		public Figure (Color color, Point leftPoint, Point rightPoint, String text)
+		{
+			Color = color;
+			Text = text;
+			LeftPoint = leftPoint;
+			RightPoint = rightPoint;
+		}
+		public Color Color { get; set; }
+		public String Text { get; set; }
+		public Int16[] Options { get; set; }
+		public Point LeftPoint { get; set; }
+		public Point RightPoint { get; set; }
+		public List<Point> ConnectionPoint { get; set; }
+
+		public abstract void Draw();
+
+	}
+
+}
---
+++ VectorRed/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorRed
+{
+	class Rectangle : Figure 
+	{
+		private System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle();
+		public Rectangle(Color color, Point leftPoint, Point rightPoint, String text): base(color, leftPoint, rightPoint, text)
+		{ }
+		public override void Draw()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
