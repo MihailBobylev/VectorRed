2a0c8de03edee2ba06d3a1ef1713e8fb6515f007
mihail.bobylev.2017 <mihail.bobylev.2017@mail.ru>
2020-11-15 14:23:49 +0300

    Созданы новые классы

---
+++ VectorRed/Connection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorRed
+{
+
+	struct ConnectionEnd
+	{
+		Figure LinkedFigure;
+		int PointIndex;
+		PointF Coords;
+		public ConnectionEnd(Figure fig, int idx) 
+		{
+			LinkedFigure = fig;
+			PointIndex = idx;
+			Coords = new PointF();
+		}
+		public ConnectionEnd(PointF p)
+		{
+			LinkedFigure = null;
+			PointIndex = -1;
+			Coords = p;
+		}
+		public PointF Target
+		{
+			get
+			{
+				if (LinkedFigure == null)
+					return Coords;
+				else
+					return LinkedFigure.ToGlobal(LinkedFigure.ConnectionPoints[PointIndex]);
+			}
+		}
+	}
+
+	class Connection : IHasOutline
+	{
+		public ConnectionEnd Start;
+		public ConnectionEnd End;
+
+		public Connection() { }
+		public Pen Outline { get; set; }
+	}
+}
---
+++ VectorRed/Diagramm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorRed
+{
+	class Diagramm
+	{
+		private List<Figure> figures;
+		private List<Connection> connections;
+		private List<Figure> selectedF;
+		private List<Connection> selectedC;
+		public Diagramm() 
+		{
+			Figures = new List<Figure>();
+			Connections = new List<Connection>();
+			SelectedF = new List<Figure>();
+			SelectedC = new List<Connection>();
+		}
+
+		internal List<Figure> Figures { get => figures; set => figures = value; }
+		internal List<Connection> Connections { get => connections; set => connections = value; }
+		internal List<Figure> SelectedF { get => selectedF; set => selectedF = value; }
+		internal List<Connection> SelectedC { get => selectedC; set => selectedC = value; }
+	}
+}
